60408d0a74be343d3c114abad888a69f675b6f5e
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-01 11:31:29 +0100

    GET product and products list

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -4,13 +4,23 @@ const router = express.Router();
 const { Product } = require('../models/product');
 
 router.get('/', async (req, res) => {
-  const productList = await Product.find();
+  const productList = await Product.find().select('name image -_id');
 
   if (!productList) {
     res.status(500).json({ success: false });
   }
 
   res.send(productList);
+});
+
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(product);
 });
 
 router.post('/', async (req, res) => {
