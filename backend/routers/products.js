655c357654ce393b768227eb61f90d9e0239f1e6
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-01 11:40:29 +0100

    Populate category in products

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { Product } = require('../models/product');
 
 router.get('/', async (req, res) => {
-  const productList = await Product.find().select('name image -_id');
+  const productList = await Product.find().populate('category');
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate('category');
 
   if (!product) {
     res.status(500).json({ success: false });
