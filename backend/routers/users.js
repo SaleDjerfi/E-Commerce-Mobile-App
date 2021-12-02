3b433551cdd403d640b7e555e8447542c1ab4e1a
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-02 13:58:58 +0100

    Register fix

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -70,26 +70,23 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  const secret = process.env.secret;
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
 
-  if (!user) {
-    return res.status(400).send('The user not found');
-  }
+  if (!user) return res.status(400).send('the user cannot be created!');
 
-  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-    const token = jwt.sign(
-      {
-        userId: user.id,
-      },
-      secret,
-      { expiresIn: '1d' }
-    );
-
-    res.status(200).send({ user: user.email, token: token });
-  } else {
-    return res.status(400).send('password is wrong');
-  }
+  res.send(user);
 });
 
 module.exports = router;
