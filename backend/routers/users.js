773ca6f2f3d48a4b2cd3ee1b8002047cd9f35f93
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-02 12:16:26 +0100

    Login user and creating token

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) => {
   const userList = await User.find().select('-passwordHash');
@@ -42,6 +43,29 @@ router.post('/', async (req, res) => {
   if (!user) return res.status(400).send('The user cannot be created');
 
   res.send(user);
+});
+
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.secret;
+
+  if (!user) {
+    return res.status(400).send('The user not found');
+  }
+
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      secret,
+      { expiresIn: '1d' }
+    );
+
+    res.status(200).send({ user: user.email, token: token });
+  } else {
+    return res.status(400).send('password is wrong');
+  }
 });
 
 module.exports = router;
