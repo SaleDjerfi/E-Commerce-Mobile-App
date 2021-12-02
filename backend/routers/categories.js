6bd16ac30d560026abe1246135da10659942cc18
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-02 10:45:04 +0100

    New users API

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -30,7 +30,7 @@ router.post('/', async (req, res) => {
   });
   category = await category.save();
 
-  if (!category) return res.status(404).send('The category cannot be created');
+  if (!category) return res.status(400).send('The category cannot be created');
 
   res.send(category);
 });
