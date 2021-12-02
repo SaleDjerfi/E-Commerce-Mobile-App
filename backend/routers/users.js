fc93d7ae20a91c597f053265dd00684c352624bd
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-02 11:41:17 +0100

    GET user and userList(excluding password)

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -3,14 +3,25 @@ const router = express.Router();
 const { User } = require('../models/user');
 const bcrypt = require('bcryptjs');
 
-router.get('/', async (req, res) => {
-  const userList = await Product.find();
+router.get(`/`, async (req, res) => {
+  const userList = await User.find().select('-passwordHash');
 
   if (!userList) {
     res.status(500).json({ success: false });
   }
 
   res.send(userList);
+});
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id).select('-passwordHash');
+
+  if (!user) {
+    res
+      .status(500)
+      .json({ message: 'The catrgory with the given ID was not found' });
+  }
+  res.status(200).send(user);
 });
 
 router.post('/', async (req, res) => {
