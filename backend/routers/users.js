d1ec198f63d83af579a279b1d7312caebb4d1da9
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-12-02 14:19:56 +0100

    Delete users and users count

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -89,4 +89,33 @@ router.post('/register', async (req, res) => {
   res.send(user);
 });
 
+router.delete('/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res
+          .status(200)
+          .json({ success: true, message: 'user is deleted' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'user not found' });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
+router.get(`/get/count`, async (req, res) => {
+  const userCount = await User.countDocuments();
+
+  if (!userCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    userCount: userCount,
+  });
+});
+
 module.exports = router;
